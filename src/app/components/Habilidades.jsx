8337074087c4a2de594f6c2a9d1fbd35e5010d68
Karen Verdugo <kverdugo@voxcentrix.net>
2024-07-18 19:49:50 -0700

    made it more responsive

--- src/app/components/Habilidades.jsx
+++ src/app/components/Habilidades.jsx
@@ -1,49 +1,53 @@
 import React from "react";
-import { RiNextjsFill } from "react-icons/ri";
+import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
+import { PiFigmaLogoFill } from "react-icons/pi";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact, FaCss3Alt, FaHtml5, FaGitAlt } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
 
 const Habilidades = () => {
   return (
-    <section id="habilidades" className="pb-12">
-      <h1 className="text-white text-5xl text-center mb-4">Habilidades</h1>
+    <section id="habilidades" className="pb-12 px-4">
+      <h1 className="text-white text-4xl md:text-5xl text-center mb-4">Habilidades</h1>
       <div className="flex items-center justify-center mb-4">
         <img src="/images/linea.png" alt="linea" className="max-w-full h-auto" />
       </div>
       {/* Icons */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 mx-48 items-center justify-center">
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/html.svg" alt="HTML 5" className="h-12 w-12 mb-2" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <FaHtml5 className="h-10 w-10 mb-2 text-orange-500" />
           HTML 5
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/css.svg" alt="CSS 3" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <FaCss3Alt className="h-10 w-10 mb-2 text-blue-600" />
           CSS 3
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/typescript.svg" alt="TypeScript" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <SiTypescript className="h-10 w-10 mb-2 text-blue-400" />
           TypeScript
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <RiNextjsFill className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <RiNextjsFill className="h-10 w-10 mb-2" />
           Next.js
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/git.svg" alt="Git" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <FaGitAlt className="h-10 w-10 mb-2 text-orange-700" />
           Git
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/javascript.svg" alt="JavaScript" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <IoLogoJavascript className="h-10 w-10 mb-2 text-yellow-300" />
           JavaScript
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/reactjs.svg" alt="React" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <FaReact className="h-10 w-10 mb-2 text-blue-400" />
           React
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/figma.svg" alt="Figma" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <PiFigmaLogoFill className="h-10 w-10 mb-2 text-purple-400" />
           Figma
         </div>
-        <div className="flex flex-col items-center justify-center text-xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
-          <img src="/images/icons/tailwindcss.svg" alt="TailwindCSS" className="h-12 w-12 mb-2" />
+        <div className="flex flex-col items-center justify-center text-lg sm:text-xl md:text-2xl lg:text-4xl text-white p-4 border border-[#1E293B] rounded-xl">
+          <RiTailwindCssFill className="h-10 w-10 mb-2 text-[#009688]" />
           TailwindCSS
         </div>
       </div>
--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,21 +1,57 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Container from "./Container";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
+import { Menu, X } from 'react-feather';
 
 const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLinkClick = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <header className="w-full sticky top-0 z-50 shadow-xl shadow-[#111827]-100 bg-[#111827]">
       <nav>
-        <Container className="relative z-50 flex justify-between py-8 px-12">
+        <Container className="relative z-50 flex items-center justify-between py-8 px-12">
           {/* Logo */}
           <div className="relative z-10">
             <Logo />
           </div>
-          {/* NavLinks */}
-          <div>
-            <NavLinks className="hidden lg:flex lg:gap-10 items-center" />
+          {/* Mobile Menu Button */}
+          <button
+            className="lg:hidden text-white focus:outline-none"
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
+          {/* NavLinks for larger screens */}
+          <div className="hidden lg:flex lg:gap-10 items-center">
+            <NavLinks />
+          </div>
+          {/* Mobile Menu Links */}
+          <div
+            className={`lg:hidden fixed top-0 left-0 w-full h-full bg-[#111827] transition-transform duration-300 transform ${
+              isMobileMenuOpen ? 'translate-x-0' : 'translate-x-full'
+            }`}
+          >
+            <div className="flex justify-end p-4">
+              <button
+                className="text-white"
+                onClick={toggleMobileMenu}
+              >
+                <X size={24} />
+              </button>
+            </div>
+            <div className="flex flex-col items-center mt-12">
+              <NavLinks className="flex flex-col gap-4 text-white" onClick={handleLinkClick} />
+            </div>
           </div>
         </Container>
       </nav>
